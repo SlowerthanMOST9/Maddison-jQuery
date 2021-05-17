5062a82004674cc6d8cca8531f40194d42f43326
Logan Madej <lnmadej23@gmail.com>
2021-05-17 08:52:59 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+    $("#cart").on("click", () => {
+      $("#cartMenu").show();
+    });
+  
+    $("#account").on("click", () => {
+      $("#accountMenu").show();
+    });
+  
+    $("#help").on("click", () => {
+      $("#helpMenu").show();
+    });
+  
+    $("#cart").on("mouseleave", () => {
+      $("#cartMenu").hide();
+    });
+  
+    $("#account").on("mouseleave", () => {
+      $("#accountMenu").hide();
+    });
+  
+    $("#help").on("mouseleave", () => {
+      $("#helpMenu").hide();
+    });
+  });
